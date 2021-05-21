1e0d48762a461a319919e7f18da61526e2fb2a69
Charles Grant Nichols <grant@inkdots.com>
2021-05-21 13:37:25 -0500

    commit tree c3925a5070f4a7e1252b4e90bfd81a8a7de76e85

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,25 @@
+require 'pry'
 
+class Genre
+
+    attr_accessor :name
+
+    @@all = []
+
+   def initialize(name)
+        @name = name
+        @@all << self
+   end
+
+   def self.all
+    @@all
+   end
+
+   def songs
+    Song.all {| song | song.genre == self}
+   end
+
+   def artists
+    songs.map {|genre| genre.artist}
+   end
+end
